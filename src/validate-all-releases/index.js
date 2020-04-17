65651dba0e42ba68a1facd81889e7ff3a2069f50
mozafar <kabaros@gmail.com>
2020-04-17 15:52:25 +0100

    Use absolute path for running shell script

--- src/validate-all-releases/index.js
+++ src/validate-all-releases/index.js
@@ -5,9 +5,9 @@ module.exports = args => {
   require('./extract-releases')(args)
 
   // Validate the MD5 checksum for all releases
+  console.log(__dirname)
   if (
-    shell.exec(`bash ./src/validate-all-releases/check-md5.sh < ${args.output}`)
-      .code !== 0
+    shell.exec(`bash ${__dirname}/check-md5.sh < ${args.output}`).code !== 0
   ) {
     shell.echo('Error: validation of releases failed')
     shell.exit(1)
