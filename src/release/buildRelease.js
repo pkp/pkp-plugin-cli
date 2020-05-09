d01969e8890dcb2baec7f0609cba0547d28a4540
mozafar <kabaros@gmail.com>
2020-05-09 16:10:32 +0100

    Add ability to run custom build commands

--- src/release/buildRelease.js
+++ src/release/buildRelease.js
@@ -1,13 +1,9 @@
-const { error, log } = require('../utils/log')
+const { error, log, info } = require('../utils/log')
 const inquirer = require('inquirer')
 const execa = require('execa')
 const tar = require('tar')
 
-const choices = [
-  'No build step',
-  'composer install (not implemented yet)',
-  'gulp build (not implemented yet)'
-]
+const choices = ['No build step', 'composer install', 'gulp build']
 
 module.exports = async ({ pluginName: fileName }) => {
   const { buildCommand } = await inquirer.prompt([
@@ -19,6 +15,21 @@ module.exports = async ({ pluginName: fileName }) => {
       choices
     }
   ])
+
+  if (buildCommand !== choices[0]) {
+    try {
+      info(`Running the command: ${buildCommand}`)
+      const [command, ...restArgs] = buildCommand.split(' ')
+      console.log(command, restArgs)
+      const { stderrr } = await execa(command, restArgs)
+      error(stderrr)
+    } catch (err) {
+      error(err)
+      // ToDo: make running build more sophisticated
+      log('ignoring the build error')
+    }
+  }
+
   log(`Creating tar file..`)
 
   const { stdout: tempFileName } = await execa('mktemp', `${fileName}XXXXXXXX`)
