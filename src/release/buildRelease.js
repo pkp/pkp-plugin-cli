d46e49c90e647d1f575cbad9b8ea6b48396cd0df
mozafar <kabaros@gmail.com>
2020-06-18 13:15:36 +0100

    Change build release phase to automatically detect gulp or composer file

--- src/release/buildRelease.js
+++ src/release/buildRelease.js
@@ -10,36 +10,42 @@
  * - prompts the user to run any extra build commands in the current folder (gulp build, composer install etc..)
  * - creates a Tar file from the current folder (in preparation to uploading it as an asset to the Github release)
  */
-const { error, log, info } = require('../utils/log')
+const { error, log, info, debug } = require('../utils/log')
 const inquirer = require('inquirer')
 const execa = require('execa')
 const tar = require('tar')
+const fs = require("fs");
 
 const choices = ['No build step', 'composer install', 'gulp build']
 
 module.exports = async ({ pluginName: fileName }) => {
-  const { buildCommand } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'buildCommand',
-      message:
-        'What build command do you want to run before pushing  the release?',
-      choices
-    }
-  ])
+  try {
+    info('Building the release')
+    debug('Running submodule update command')
+    const { stderr: submoduleUpdateError } = await execa('git', ['submodule', 'update', '--init', '--recursive'])
+    debug(submoduleUpdateError)
+    debug('Finished submodule update')
 
-  if (buildCommand !== choices[0]) {
-    try {
-      info(`Running the command: ${buildCommand}`)
-      const [command, ...restArgs] = buildCommand.split(' ')
-      console.log(command, restArgs)
-      const { stderrr } = await execa(command, restArgs)
-      error(stderrr)
-    } catch (err) {
-      error(err)
-      // ToDo: make running build more sophisticated
-      log('ignoring the build error')
+    if (fs.existsSync('./composer.json')) {
+      info('Composer file detected. The tool will run "composer install".')
+      const { stderr } = await execa('composer', ['install'])
+      error(stderr)
+    } else {
+      info('No composer.json file found')
     }
+
+    if (fs.existsSync('./gulpfile.js')) {
+      info('Gulp file detected. The tool will run "gulp build".')
+      const { stderr } = await execa('gulp', ['build'])
+      error(stderr)
+    } else {
+      debug('No gulp file file found')
+    }
+
+  } catch (err) {
+    error(err)
+    // ToDo: make running build more sophisticated
+    log('An error happened while trying to run the build commands. It will be ignored, you can run any necessary build commands manually then rerun this tool.')
   }
 
   log(`Creating tar file..`)
