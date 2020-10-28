32de4f563be2bbbf1ba0ed0077c607532587895b
mozafar <kabaros@gmail.com>
2020-10-28 17:38:30 +0000

    Change structure of generated site
    to have releases as columns

--- src/generate-site/extractAllPlugins.js
+++ src/generate-site/extractAllPlugins.js
@@ -13,16 +13,15 @@
  */
 const xml2js = require('xml2js')
 
-const { readFile } = require('../files')
-const { info, debug, error } = require('../log')
+const { readFile } = require('../utils/files')
+const { debug, error } = require('../utils/log')
 
 const parser = new xml2js.Parser()
 
-const extractData = async filePath => {
+const extractData = async (filePath, releaseVersions) => {
   debug(`Extracting releases from ${filePath}`)
 
   const xml = await readFile(filePath)
-  const releases = []
 
   const result = await parser.parseStringPromise(xml)
 
@@ -31,37 +30,48 @@ const extractData = async filePath => {
   const plugins = result.plugins.plugin.map(plugin => {
     if (!plugin.name) {
       error(`${JSON.stringify(plugin)}\n`)
-      throw 'The last plugin does not have have a name attribute'
+      throw new Error('The last plugin does not have have a name attribute')
     }
 
     const releases = plugin.release.map(release => {
-      const expectedMd5Sum = release.$.md5
       const version = release.$.version
       const date = release.$.date
 
-
+      // console.log(release)
 
       const compatibility = release.compatibility.map(compat => {
-        return compat.$.application + ' ' + compat.version.join(', ')
+        return compat.version
       })
+      var allCompatibilities = [].concat.apply([], compatibility).join(', ')
 
       return {
         version,
         date,
         url: release.package[0],
-        compatibility
+        compatibility: allCompatibilities
       }
     })
 
-    releases.sort(function (a, b) { return new Date(b.date) - new Date(a.date) });
+    releases.sort(function (a, b) { return new Date(b.date) - new Date(a.date) })
+
+    const branchesWithPackages = releaseVersions.map(version => {
+      const matchingRelease = releases.find(r => {
+        return r.compatibility.match(version)
+      })
+      console.log(matchingRelease)
+      return matchingRelease
+    })
 
     return {
       // name: plugin.name[0]._,
-      name: plugin.homepage[0].replace('https://github.com/', '').replace(/\/$/, ''),
+      name: plugin.homepage[0].replace('https://github.com/', '').replace(/\/$/, '').replace('/', ' / '),
       url: plugin.homepage[0],
+      branchesWithPackages,
       releases: releases
     }
   })
+
+  // console.log(plugins)
 
   return plugins
 }
